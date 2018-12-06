9b45ec145d0990ff1ee610fccaaeff35a232b68a
Nikos Chan <nik0sc@users.noreply.github.com>
2018-12-06 12:10:09 +0800

    Modify frontend interface to depend on geopoint instead of own coordinates class

--- app/src/main/java/com/example/kohseukim/clientside/FrontEnd.java
+++ app/src/main/java/com/example/kohseukim/clientside/FrontEnd.java
@@ -1,50 +1,26 @@
 package com.example.kohseukim.clientside;
 
+import com.google.firebase.firestore.GeoPoint;
+
 import java.util.List;
-
-class Coordinates {
-    private double lat;
-    private double lon;
-
-    public double getLat() {
-        return lat;
-    }
-
-    public void setLat(double lat) {
-        this.lat = lat;
-    }
-
-    public double getLon() {
-        return lon;
-    }
-
-    public void setLon(double lon) {
-        this.lon = lon;
-    }
-
-    public Coordinates(double lat, double lon) {
-        this.lat = lat;
-        this.lon = lon;
-    }
-}
 
 public interface FrontEnd {
     enum AlertType {
         LEVEL_1, LEVEL_2
     }
 
-    public void showAlert(AlertType type);
-    public void dropAlert(AlertType type);
+    void showAlert(AlertType type);
+    void dropAlert(AlertType type);
 
-    public void showAmbulance(double lat, double lon, double heading);
-    public void updateAmbulance(double lat, double lon, double heading);
-    public void dropAmbulance();
+    void showAmbulance(GeoPoint location, double heading);
+    void updateAmbulance(GeoPoint location, double heading);
+    void dropAmbulance();
 
-    public void showRadius(double radius);
-    public void dropRadius();
+    void showRadius(double radius);
+    void dropRadius();
 
-    public void showRoute(List<Coordinates> route);
-    public void updateRoute(List<Coordinates> route);
-    public void dropRoute();
+    void showRoute(List<GeoPoint> route);
+    void updateRoute(List<GeoPoint> route);
+    void dropRoute();
 }
 
