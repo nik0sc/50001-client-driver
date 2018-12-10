d9be7ca9d837cfd4837cdc24d5d91c9f36059f1a
skimkoh <kohseukim@gmail.com>
2018-12-10 23:53:06 +0800

    Remove all buttons for testing purposes, added app icon

--- app/src/main/java/com/example/kohseukim/clientside/MapsActivity.java
+++ app/src/main/java/com/example/kohseukim/clientside/MapsActivity.java
@@ -96,7 +96,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Reference to level 1 button
         //Button map_btn_one = findViewById(R.id.map_btn_one);
 
-        final MediaPlayer levelonealertsound = MediaPlayer.create(this, R.raw.levelonealertsound);
+        //final MediaPlayer levelonealertsound = MediaPlayer.create(this, R.raw.levelonealertsound);
 
 
         // when the button is clicked, we go to the level 1 alert
@@ -114,20 +114,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 //
 //
 //        // Reference to level 2 button
-        Button map_btn_two = findViewById(R.id.map_btn_two);
+        //Button map_btn_two = findViewById(R.id.map_btn_two);
 
         //final MediaPlayer levelTwoAlertSound = MediaPlayer.create(this, R.raw.leveltwoalert);
 
 
         // when the button is clicked, we ego to the level 2 alert
-        map_btn_two.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MapsActivity.this, LevelTwoActivity.class);
-                startActivity(i);
-                overridePendingTransition(0, 0);
-            }
-        });
+//        map_btn_two.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(MapsActivity.this, LevelTwoActivity.class);
+//                startActivity(i);
+//                overridePendingTransition(0, 0);
+//            }
+//        });
 
         //frontend = new FrontendImpl();
 
